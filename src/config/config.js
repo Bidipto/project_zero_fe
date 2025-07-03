80fd18d63a22265093d869e5d50f1ca5a8af152b
anubhav126 <tinfinite1248@gmail.com>
2025-07-04 02:04:57 +0530

    some more fixes

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,4 @@
 class EnvironmentVariables {
   static BACKEND_URL = process.env.BACKEND_URL || 'http://127.0.0.1:8000';
-  static JWT_SECRET = process.env.JWT_SECRET
 }
 export default EnvironmentVariables;    
