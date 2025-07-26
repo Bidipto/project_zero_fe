dc59d16d7ea22a1ae13fa7881bb8d76020a4aea9
anubhav126 <tinfinite1248@gmail.com>
2025-07-26 20:33:29 +0530

    added login UI

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,10 @@
 class EnvironmentVariables {
   static BACKEND_URL = process.env.BACKEND_URL || 'http://127.0.0.1:8000';
+  static GITHUB_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
+  static GITHUB_REDIRECT_URI = process.env.NEXT_PUBLIC_GITHUB_REDIRECT_URI;
+  static GITHUB_CLIENT_SECRET = process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET;
+  static GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  static GOOGLE_REDIRECT_URI = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI;
+  static GOOGLE_CLIENT_SECRET = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET;
 }
 export default EnvironmentVariables;    
